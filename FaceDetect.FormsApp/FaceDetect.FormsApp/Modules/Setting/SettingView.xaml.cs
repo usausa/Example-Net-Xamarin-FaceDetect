fd621aff232c77ea6e5261d5fc8d2b9f36dd4de8
usausa <machi.pon@gmail.com>
2021-04-11 19:55:54 +0900

    Update

---
+++ FaceDetect.FormsApp/FaceDetect.FormsApp/Modules/Setting/SettingView.xaml.cs
@@ -0,0 +1,13 @@
+namespace FaceDetect.FormsApp.Modules.Setting
+{
+    using Smart.Navigation.Attributes;
+
+    [View(ViewId.Setting)]
+    public partial class SettingView
+    {
+        public SettingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- FaceDetect.FormsApp/FaceDetect.FormsApp/Modules/Setting/SettingViewModel.cs
+++ FaceDetect.FormsApp/FaceDetect.FormsApp/Modules/Setting/SettingViewModel.cs
@@ -1,6 +1,49 @@
 namespace FaceDetect.FormsApp.Modules.Setting
 {
-    public class SettingViewModel
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+
+    using Smart.ComponentModel;
+    using Smart.Navigation;
+
+    public class SettingViewModel : AppViewModelBase
     {
+        public NotificationValue<bool> CanScan { get; } = new();
+
+        public ICommand ScanCommand { get; }
+
+        public ICommand BackCommand { get; }
+
+        public SettingViewModel(
+            ApplicationState applicationState)
+            : base(applicationState)
+        {
+            ScanCommand = MakeAsyncCommand<string>(ExecuteScan);
+
+            BackCommand = MakeAsyncCommand(OnNotifyBackAsync);
+        }
+
+        public override void OnNavigatingFrom(INavigationContext context)
+        {
+            CanScan.Value = false;
+        }
+
+        public override void OnNavigatingTo(INavigationContext context)
+        {
+            CanScan.Value = true;
+        }
+
+        protected override Task OnNotifyBackAsync()
+        {
+            return Navigator.ForwardAsync(ViewId.Menu);
+        }
+
+        private async Task ExecuteScan(string text)
+        {
+            // TODO
+            Debug.WriteLine(text);
+            await Task.Delay(1000);
+        }
     }
 }
